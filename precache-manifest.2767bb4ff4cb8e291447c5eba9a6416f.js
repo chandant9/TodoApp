e7683e60e1256810f2567a5f38c3ea29264f5707
chandant9 <discoverchan9@gmail.com>
2020-08-28 12:21:25 +0400

    Updates

---
+++ precache-manifest.2767bb4ff4cb8e291447c5eba9a6416f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3af4923d7a4a8cdbb440ce396e34aba",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "96406a8fac45842fb65c",
+    "url": "/TodoApp/static/css/main.6df34b0a.chunk.css"
+  },
+  {
+    "revision": "86162ffa9dc9f9ed1b91",
+    "url": "/TodoApp/static/js/2.9b57d343.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.9b57d343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96406a8fac45842fb65c",
+    "url": "/TodoApp/static/js/main.6e7ac8c1.chunk.js"
+  },
+  {
+    "revision": "6178001199e45a76af3f",
+    "url": "/TodoApp/static/js/runtime-main.579f7b68.js"
+  }
+]);
